425c16204ddf67aa5856ee61caf42d0e1f322fa5
Thomas Varsamidis <thomas@varsamidis.com>
2026-04-03 18:35:30 +0300

    Add test class s08

---
+++ src/test/java/gr/codehub/unit/other/s08OrderingAndLifecycleDemo.java
@@ -0,0 +1,89 @@
+package gr.codehub.unit.other;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class s08OrderingAndLifecycleDemoTest {
+
+    static Integer aValue = 5;
+
+    @BeforeAll
+    static void setUp() {
+        //this will run at the very beginning
+        System.out.println("Starting the process with value:" + aValue);
+    }
+
+    @Test
+    @Order(2)
+    void shouldIncrementValueBySecond() {
+        aValue++;
+        assertEquals(6, aValue, "Second test for aValue++");
+    }
+
+    @Test
+    @Order(3)
+    void shouldIncrementValueByFirst() {
+        aValue++;
+        assertEquals(6, aValue, "First test for aValue++");
+    }
+
+
+    @AfterAll
+    static void tearDown() {
+        System.out.println("Ending the process with value:" + aValue);
+    }
+
+
+    @Nested
+    @DisplayName("Nested Test Class")
+    @Order(1)
+    class NestedClass {
+
+        String myString;
+
+        @BeforeEach
+        void initializeString() {
+            System.out.println("Initializing string");
+            myString = "Hello World";
+        }
+
+        @Test
+        @DisplayName("Clear the content of string")
+        void shouldClearString() {
+            System.out.println("Clearing string");
+            myString = "";
+            assertTrue(myString.isEmpty());
+        }
+
+        @Test
+        @DisplayName("Check string length")
+        void shouldCheckStringLength() {
+            System.out.println("Checking the length of string");
+            String newString = myString.replace("World", "");
+            assertEquals(6, newString.length());
+        }
+
+        @AfterEach
+        public void cleanUp() {
+            System.out.println("A clean up process for the inner class");
+        }
+
+    }
+
+    @AfterEach
+    public void cleanUp() {
+        System.out.println("A clean up process for the outer class");
+    }
+}
